02c3c55872539e4184ecd81a8ae49a601116056e
Antonio Guzman <aguzmanv09@gmail.com>
2021-10-14 18:53:59 -0500

    Initial html structure and add variables and sass config

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBU5omdfu-3aa04NgmQIhIp4sgzNybUc5U",
+  authDomain: "invoice-generator-cbb07.firebaseapp.com",
+  projectId: "invoice-generator-cbb07",
+  storageBucket: "invoice-generator-cbb07.appspot.com",
+  messagingSenderId: "648307851518",
+  appId: "1:648307851518:web:4587ad24f312af8e1d65af"
+};
+
+const app = initializeApp(firebaseConfig);
