65eac6bd63ba96656c11049f8aaa2e2c847f3184
Antonio Guzman <aguzmanv09@gmail.com>
2021-10-16 17:14:37 -0500

    Update product list and add logic to do the invoice

--- js/index.js
+++ js/index.js
@@ -20,13 +20,52 @@ const db = getFirestore(app);
 const getProducts = async () => {
   const productsCol = collection(db, 'products');
   const productSnapshot = await getDocs(productsCol);
-  const productList = productSnapshot.docs.map((doc) => doc.data());
+  const productList = productSnapshot.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id }
+  });
   return productList;
 };
 
+const cart = [];
+
+const addItem = (el, count) => {
+  if (count >= 10) {
+    el.innerText = count;
+    return count;
+  }
+  el.innerText = count + 1;
+  return count + 1;
+}
+
+const removeItem = (el, count) => {
+  if (count <= 1) {
+    el.innerText = count;
+    return count;
+  }
+  el.innerText = count - 1;
+  return count - 1;
+}
+
+const addToCart = ({ product, quantity }) => {
+  const isInCart = cart.find((item) => item.id === product.id)
+  if (isInCart) {
+    isInCart.quantity += quantity;
+    return;
+  }
+  cart.push({ ...product, quantity });
+}
+
+const removeToCart = ({ id }) => {
+  
+}
+
 const productCard = ({ id, product, src, price }) => {
+  let count = 1;
+  const obj = { id, name: product, price, image: src };
   const container = document.createElement('div');
   const body = document.createElement('div');
+  const counterContainer = document.createElement('div');
+  const counter = document.createElement('p');
   const footer = document.createElement('div');
   const imageContainer = document.createElement('figure');
   const image = document.createElement('img');
@@ -34,6 +73,8 @@ const productCard = ({ id, product, src, price }) => {
   const name = document.createElement('strong');
   const priceEl = document.createElement('p');
   const cta = document.createElement('button');
+  const add = document.createElement('button');
+  const remove = document.createElement('button');
 
   container.className = 'ProductCard';
   body.className = 'ProductCard__body';
@@ -41,6 +82,9 @@ const productCard = ({ id, product, src, price }) => {
   imageContainer.className = 'ProductCard__image';
   nameContainer.className = 'single-line';
   cta.className = 'btn-outline';
+  add.className = 'btn-link';
+  remove.className = 'btn-link';
+  counterContainer.className = 'ProductCard__body--counter';
 
   image.src = src;
   image.alt = product;
@@ -49,21 +93,36 @@ const productCard = ({ id, product, src, price }) => {
   name.innerText = product;
   priceEl.innerHTML = `Price: $${price}`;
   cta.type = 'button';
-  cta.innerText = 'Comprar';
+  cta.innerText = 'Agregar';
+  add.type = 'button';
+  add.innerText = '+';
+  remove.type = 'button';
+  remove.innerText = '-';
+  counter.innerHTML = count;
 
+  counterContainer.appendChild(remove)
+  counterContainer.appendChild(counter)
+  counterContainer.appendChild(add)
   imageContainer.appendChild(image);
   nameContainer.appendChild(name);
   body.appendChild(nameContainer);
   body.appendChild(priceEl);
+  body.appendChild(counterContainer)
   footer.appendChild(cta);
   container.appendChild(imageContainer);
   container.appendChild(body);
   container.appendChild(footer);
 
+  add.addEventListener('click', () => count = addItem(counter, count));
+  remove.addEventListener('click', () => count = removeItem(counter, count));
+  cta.addEventListener('click', () => addToCart({ product: obj, quantity: count }))
+
   return container;
 };
 
 const list = document.getElementById('List');
+const modal = document.getElementById('Modal');
+const btnCard = document.getElementById('CartBtn');
 
 const products = getProducts();
 
@@ -82,4 +141,104 @@ products.then((data) => {
     list.innerHTML = 'No hay elementos'
   }
 });
+
+const showModal = () => {
+  modal.style.display = 'grid';
+}
+
+const invoiceHeader = () => {
+  const component = document.createElement('div');
+  const item = document.createElement('p');
+  const total = document.createElement('p');
+  const count = document.createElement('p');
+  const cost = document.createElement('p');
+
+  component.className = 'InvoiceModal__body--header'
+
+  item.innerText = 'Product';
+  count.innerText = 'Quantity';
+  cost.innerText = 'Price';
+  total.innerText = 'Total';
+
+  component.appendChild(item);
+  component.appendChild(cost);
+  component.appendChild(count);
+  component.appendChild(total);
+  return component;
+}
+
+const invoiceItem = ({ product, quantity, price }) => {
+  const component = document.createElement('div');
+  const item = document.createElement('p');
+  const total = document.createElement('p');
+  const count = document.createElement('p');
+  const cost = document.createElement('p');
+
+  component.className = 'InvoiceModal__body--item'
+
+  item.innerText = product;
+  count.innerText = quantity;
+  cost.innerText = `$${price}`;
+  total.innerText = `$${price * quantity}`;
+
+  component.appendChild(item);
+  component.appendChild(cost);
+  component.appendChild(count);
+  component.appendChild(total);
+  return component;
+}
+
+const invoiceFooter = () => {
+  const container = document.createElement('div');
+  const total = document.createElement('p');
+  const list = cart.map((item) => item.price * item.quantity);
+  const sum = list.reduce((a, b) => a + b);
+
+  container.className = 'InvoiceModal__body--footer';
+
+  console.log(sum);
+  total.innerText = `Total to pay: $${sum}`;
+
+  container.appendChild(total);
+  return container;
+}
+
+const showInvoice = () => {
+  const container = document.createElement('section');
+  const header = document.createElement('section');
+  const body = document.createElement('section');
+  const headerTitle = document.createElement('p');
+  const headerClose = document.createElement('button');
+
+  container.className = 'InvoiceModal';
+  header.className = 'InvoiceModal__header';
+  body.className = 'InvoiceModal__body';
+  headerClose.className = 'btn-link';
+
+  headerTitle.innerText = 'Factura';
+  headerClose.innerText = 'x';
+
+  body.appendChild(invoiceHeader());
+  cart.map((item) => {
+    body.appendChild(invoiceItem({
+      price: item.price,
+      product: item.name,
+      quantity: item.quantity,
+    }));
+    return item;
+  });
+  body.appendChild(invoiceFooter());
+
+  headerClose.addEventListener('click', () => modal.removeChild(container));
+
+  header.appendChild(headerTitle);
+  header.appendChild(headerClose);
+  container.appendChild(header);
+  container.appendChild(body);
+  modal.appendChild(container);
+  showModal();
+}
+
+btnCard.addEventListener('click', showInvoice)
+modal.addEventListener('click', () => modal.style.display = 'none')
 
